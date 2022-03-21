e972108501fd1662c8417140b38c0b36b9c6a72e
elsonly <elsonly@gmail.com>
2022-03-21 23:34:07 +0800

    feat: enhance

--- trade_view/websocket/server.py
+++ trade_view/websocket/server.py
@@ -1,13 +1,20 @@
+import math
+import time
 import asyncio
 import websockets
-# https://websockets.readthedocs.io/en/stable/
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        await websocket.send(message)
 
-async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+async def stream():
+    async with websockets.connect(
+        "ws://localhost:3000/api/live/push/sinewave_test", 
+        extra_headers={'Authorization': 'Bearer eyJrIjoiRDA2RlpjUVBQTDAzSFYzSjkwWVlvOHltRHhiUGthdHYiLCJuIjoidGVsZWdyYWYiLCJpZCI6MX0='}
+    ) as websocket:
+        i = 0
+        while True:
+            await websocket.send("test Sinewave=" + str((math.sin(math.radians(i))+1)*20+80) +" " + str(time.time_ns()+1000000000))
+            if (i == 361):
+                i = 0
+            else:
+                i = i + 15
+            await asyncio.sleep(0.03)
 
-asyncio.run(main())
+asyncio.run(stream())
