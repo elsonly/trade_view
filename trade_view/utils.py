3349d448b74579eb9e3485e3384c6678fb0503d8
elsonly <elsonly@gmail.com>
2022-08-19 09:02:46 +0800

    feat: enhance

--- trade_view/utils.py
+++ trade_view/utils.py
@@ -1,4 +1,2 @@
-
-
-def get_quote_channel(source:str, quote_type: str, code: str) -> str:
+def get_quote_channel(source: str, quote_type: str, code: str) -> str:
     return f"{source}/{quote_type}/{code}"
